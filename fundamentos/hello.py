5090c639a7c856da74c20bc6a5475d694acd4181
Kemoel Amorim Miranda <kemoel.amorim@gmail.com>
2022-03-11 09:31:29 -0300

    Entrada e saída de dados

--- fundamentos/hello.py
+++
@@ -1,7 +0,0 @@
-# Hello world
-print("Hello World")
-
-# Entrada de dado
-nome = input("Digite seu nome: ")
-# Saída de dado
-print('Seu nome é: %s' %nome)
---
+++ fundamentos/input-print.py
@@ -0,0 +1,14 @@
+
+# Entrada do dado
+nome = input("Digite seu primeiro nome: ")
+sobrenome = input('Digite seu sobrenome: ')
+
+# Saída de informação
+print('Seu primeiro nome é: %s' %nome)
+print('Seu sobrenome é: %s' %sobrenome)
+
+# Concatenação
+idade = input("Digite sua idade: ")
+print(f'Sua idade é {idade}')
+
+
